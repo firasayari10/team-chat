0e923b89542aefea56d6abbdfe445674d8531e95
firasayari10 <ayari.firas@esprit.tn>
2025-09-28 21:44:45 +0100

    bug fix : loading on landing page fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 
 "use client"
-import {UserButton} from "@/features/auth/components/user-button"
 import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
-
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces"
 import { useEffect, useMemo } from "react";
 import {useRouter} from "next/navigation"
+import { Loader } from "lucide-react";
 
 
 
@@ -24,8 +23,9 @@ export default function Home() {
         {setOpen(true);}
   },[workspaceId,isLoading,open,setOpen , router])
 return ( 
-    <div> 
-      <UserButton />
-    </div>
+    <div className="h-full flex-1 flex items-center justify-center flex-col gap-2">
+                <Loader className="size-6 animate-spoin text-muted-foreground" />
+
+            </div>
   )
 }
